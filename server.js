7f3d7376ec9a93961a98a6c313dd94546d6173d5
Hemant-Singh0 <gouravs2250@gmail.com>
2024-06-30 17:25:22 +0530

    final code

--- server.js
+++ server.js
@@ -1,30 +1,3 @@
-// const express = require('express');
-// const dotenv = require('dotenv');
-// const cors = require('cors');
-
-// dotenv.config({ path: ".env" });
-// require('./config');
-
-// const app = express();
-// app.use(cors());
-// app.use(express.json());
-
-// // Import routes
-// const authRoutes = require('./routes/authRoutes');
-// const productRoutes = require('./routes/productRoutes');
-// const cartRoutes = require('./routes/cartRoutes');
-
-// // Use routes
-// app.use('/api/auth', authRoutes);
-// app.use('/api/products', productRoutes);
-// app.use('/api/cart', cartRoutes);
-
-// const PORT = process.env.PORT;
-
-// app.listen(PORT, () => {
-//     console.log(`Example app listening on PORT ${PORT}!`);
-// });
-
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
@@ -33,15 +6,7 @@ dotenv.config({ path: ".env" });
 require('./config');
 
 const app = express();
-
-const corsOptions = {
-    origin: 'http://localhost:3000', // allow only your frontend app to access
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // allowed HTTP methods
-    allowedHeaders: ['Content-Type', 'Authorization'], // allowed headers
-    credentials: true // include credentials (e.g., cookies, authorization headers)
-};
-
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 
 // Import routes
@@ -54,8 +19,8 @@ app.use('/api/auth', authRoutes);
 app.use('/api/products', productRoutes);
 app.use('/api/cart', cartRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`Example app listening on PORT ${PORT}!`);
 });
